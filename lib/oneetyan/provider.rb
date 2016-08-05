fc4382b98953838498578ba4ca15c82da1e5091b
aereal <aereal@aereal.org>
2016-08-05 16:30:50 +0900

    Returns some oneetyan

--- lib/oneetyan/provider.rb
+++ lib/oneetyan/provider.rb
@@ -1,16 +1,23 @@
 require 'json'
+require 'rack/request'
 
 module Oneetyan
   class Provider
     def call(env)
-      body = { message: 'ok' }
-      [
-        200,
-        {
-          'content-type'           => 'application/json',
-        },
-        [JSON.generate(body)]
-      ]
+      req = Rack::Request.new(env)
+      case req.path
+      when '/oneetyan'
+        body = { oneetyan: ['cocoa'] }
+        [
+          200,
+          {
+            'content-type' => 'application/json',
+          },
+          [JSON.generate(body)]
+        ]
+      else
+        [404, {}, ['not found']]
+      end
     end
   end
 end
